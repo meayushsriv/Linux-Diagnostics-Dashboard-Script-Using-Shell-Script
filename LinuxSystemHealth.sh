d85951abd53a5a1cdb0b9eabb08fa8aa308267b1
meayushsriv <me.facteverse@gmail.com>
2025-06-26 20:01:20 +0530

    calculated metrics

--- LinuxSystemHealth.sh
+++ LinuxSystemHealth.sh
@@ -2,3 +2,8 @@
 
 HOSTNAME=$(uname -n)
 SLASH=" / "
+
+MEMORY_TOTAL=$(free -m | awk 'NR==2 {print $2 " MB"}')
+MEMORY_USE=$(free -m | awk 'NR==2 {print $3 " MB"}')
+SWAP_USE=$(free -m | awk '/^Swap/ {print $3}')
+SWAP_TOTAL=$(free -m | awk '/^Swap/ {print $2}')
